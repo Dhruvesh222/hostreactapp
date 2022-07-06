e2fdd84e93401dbd203b24be656d04d38e528c49
dhruvesh <dhruveshbhure9999@gmail.com>
2022-07-06 14:58:53 +0530

    checking after deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>deployinhg react app</h1>
+        <h1>This react app has been deployed</h1>
       </header>
     </div>
   );
